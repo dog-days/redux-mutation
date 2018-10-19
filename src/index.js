245cc764aee6f2137b5ca0b101c83a9f18e17120
xianshannan <xianshannan@qq.com>
2018-10-19 14:25:09 +0800

    修正replaceMutationObjects用法

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import {
   customStore as customBasicStore,
 } from './basic';
 import convertMutationObjects from './convert-mutation-objects';
-// import isPlainObject from './utils/isPlainObject';
+import isPlainObject from './utils/isPlainObject';
 
 /**
  * 创建经过修改后的createStore
@@ -21,9 +21,25 @@ function customStore(options) {
   if (!options) {
     options = {};
   }
+  const mutationObjectByNamespace = {};
+  /**
+   * 按照namespace的方式存放到mutationObjectByNamespace
+   * @param {...object} mutationObjects 请看文件conver-mutation-object.js注释
+   */
+  function setMutationObjectByNamespace(mutationObjects = {}) {
+    if (!Array.isArray(mutationObjects)) {
+      mutationObjects = [mutationObjects];
+    }
+    mutationObjects.forEach(mutationObject => {
+      const namespace = mutationObject.namespace;
+      if (namespace) {
+        mutationObjectByNamespace[namespace] = mutationObject;
+      }
+    });
+  }
   const { centersAliasName, ...centerOptions } = options;
   /**
-   * @param {object} mutationObjects 请看文件conver-mutation-object.js注释
+   * @param {...object} mutationObjects 请看文件conver-mutation-object.js注释
    * @param preloadedState 跟redux的createStore的一样，没做修改
    * @param enhancer 跟redux的createStore的一样，没做修改
    * @return {object} 返回了一个经过适配后的store，属性完全跟redux的store一致
@@ -33,6 +49,7 @@ function customStore(options) {
     if (typeof mutationObjects === 'function') {
       reducerAndCenters = mutationObjects;
     } else {
+      setMutationObjectByNamespace(mutationObjects);
       reducerAndCenters = convertMutationObjects(mutationObjects, {
         combineCenters,
         centersAliasName,
@@ -46,19 +63,53 @@ function customStore(options) {
     return {
       replaceMutationObjects: createReplaceMutationObjects(
         store.replaceReducerAndCenters,
-        { centersAliasName }
+        { centersAliasName, mutationObjectByNamespace }
       ),
       ...store,
     };
   };
 }
+/**
+ * @param {object} replaceReducerAndCenters 请查看./basic.js的相关注释
+ * @param {object} options.mutationObjectByNamespace mutationObject的namespace为key的对象
+ * @returns {function} replaceMutationObjects函数
+ */
 function createReplaceMutationObjects(
   replaceReducerAndCenters,
-  { centersAliasName }
+  { centersAliasName, mutationObjectByNamespace }
 ) {
   //热替换或动态加载中使用
-  return mutationObjects => {
-    const reducerAndCenters = convertMutationObjects(mutationObjects, {
+  /**
+   * @param {object | ...object} newMutationObjects 可以是单个mutationObject，也可以是多个mutationObject
+   */
+  return function(newMutationObjects) {
+    if (!Array.isArray(newMutationObjects)) {
+      newMutationObjects = [newMutationObjects];
+    }
+    // console.log(newMutationObjects);
+    newMutationObjects.forEach(mutationObject => {
+      if (!isPlainObject(mutationObject)) {
+        throw new TypeError('Expect mutationObject to be an plain object.');
+      }
+      if (!mutationObject.namespace) {
+        throw new TypeError('Expect mutationObject.namespace to be defined.');
+      }
+      if (mutationObjectByNamespace[mutationObject.namespace]) {
+        //redux reducer替换的时候，如果对应的state已经初始化了，那么initialState就会失效。
+        console.warn(
+          `The namespace "${
+            mutationObject.namespace
+          }" of mutationObject will be replaced,and the new "initialState" will lose efficacy because it has been initialized.`
+        );
+      }
+      mutationObjectByNamespace[mutationObject.namespace] = mutationObject;
+    });
+    //重新赋值
+    newMutationObjects = [];
+    for (let key in mutationObjectByNamespace) {
+      newMutationObjects.push(mutationObjectByNamespace[key]);
+    }
+    const reducerAndCenters = convertMutationObjects(newMutationObjects, {
       centersAliasName,
       combineCenters,
     });
