e8e4fe31edb037d85a745da7ac888a4fc9c80254
xianshannan <xianshannan@qq.com>
2018-11-06 15:12:06 +0800

    fix applyPlugin problem

--- src/apply-plugin.js
+++ src/apply-plugin.js
@@ -1,5 +1,3 @@
-import { compose } from 'redux';
-
 /**
  * 整合插件
  * @param {...object} plugins
@@ -13,6 +11,7 @@ export default function applyPlugin(...plugins) {
     p.centerEnhancer && centerEnhancers.push(p.centerEnhancer);
     p.reducerEnhancer && reducerEnhancers.push(p.reducerEnhancer);
     extraReducers = {
+      ...extraReducers,
       ...p.extraReducers,
     };
     if (p.extraCenters) {
@@ -25,4 +24,25 @@ export default function applyPlugin(...plugins) {
     reducerEnhancer: compose(...reducerEnhancers),
     centerEnhancer: compose(...centerEnhancers),
   };
+}
+/**
+ * 从右到左合成多个参数的函数
+ * 例如 compose(f, g, h) 合成如下面的
+ * (firstArg,...otherArgs) => f(g(h(firstArg,...otherArgs),...otherArgs),...otherArgs).
+ * 跟redux的compose区别在于，这里合并的函数是可以多个参数的，除了第一个参数一定是函数外，其他参数无限制。
+ * @param {...function} funcs 需要被合成的函数（arguments参数）
+ * @returns {function} 从右到左把接收到的函数合成后的最终函数。
+ */
+export function compose(...funcs) {
+  if (funcs.length === 0) {
+    return arg => arg;
+  }
+
+  if (funcs.length === 1) {
+    return funcs[0];
+  }
+
+  return funcs.reduce((a, b) => (firstArg, ...otherArgs) =>
+    a(b(firstArg, ...otherArgs), ...otherArgs)
+  );
 }
