014678f96cf3d65c3e33910a2f5b52333d6c6333
xianshannan <xianshannan@qq.com>
2018-11-09 15:41:54 +0800

    update .babelrc.js

--- .babelrc.js
+++ .babelrc.js
@@ -8,16 +8,13 @@ module.exports = {
         targets: {
           browsers: ['ie >= 9'],
         },
+        //不转换async 和 generator，不过项目没用到，为了减少转换后的代码量，也可以使用这两个特性
         exclude: ['transform-async-to-generator', 'transform-regenerator'],
-        modules: false,
+        //test需要把import转换成commonjs require模式
+        //rollup 支持 es import
+        modules: NODE_ENV === 'TEST' ? 'commonjs' : false,
         loose: true,
       },
     ],
   ],
-  plugins: [
-    // don't use `loose` mode here - need to copy symbols when spreading
-    '@babel/proposal-object-rest-spread',
-    //test需要把import转换成commonjs require模式
-    NODE_ENV === 'TEST' && '@babel/transform-modules-commonjs',
-  ].filter(Boolean),
 };
