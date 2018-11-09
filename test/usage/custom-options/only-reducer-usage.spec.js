ed6d9300a19c6789868160ef5bb2d895b289a0ce
xianshannan <xianshannan@qq.com>
2018-11-09 18:40:39 +0800

    add more test for reducer format

--- test/usage/custom-options/only-reducer-usage.spec.js
+++ test/usage/custom-options/only-reducer-usage.spec.js
@@ -4,14 +4,23 @@ import generatorsToAsync from 'redux-center/lib/generators-to-async';
 import { configCreateStore } from '../../../src';
 
 describe('custom options usage', () => {
+  let store;
+  let reducerSpy;
   it('should show warning when using option in only reducer mode', () => {
     const stub = sinon.stub(console, 'warn');
-    configCreateStore({
-      generatorsToAsync,
-    })(function(state = 0) {
+    reducerSpy = sinon.spy(function(state = 0) {
       return state;
     });
+    store = configCreateStore({
+      generatorsToAsync,
+    })(reducerSpy);
     stub.callCount.should.equal(1);
     stub.restore();
+  });
+  it('should work correctly using reducer format', () => {
+    const action = { type: '@@test' };
+    const prevReducerCallCount = reducerSpy.callCount;
+    store.dispatch(action);
+    reducerSpy.callCount.should.equal(prevReducerCallCount + 1);
   });
 });
