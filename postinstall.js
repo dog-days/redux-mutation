daec54a654acab725b945ad0ddfe56bc13fd9fea
xianshannan <xianshannan@qq.com>
2018-11-15 17:21:09 +0800

    update postinstall.js, postinstall will run test and build

--- postinstall.js
+++ postinstall.js
@@ -4,7 +4,13 @@ const spawn = require('cross-spawn');
 
 //根据是否存在.gitignore判断是否postinstall，发布后.gitignore不会一起发布
 if (fs.existsSync(path.resolve(process.cwd(), '.gitignore'))) {
-  spawn('npm', ['run', 'bootstrap'], {
+  spawn.sync('npm', ['run', 'bootstrap'], {
+    stdio: 'inherit',
+  });
+  spawn.sync('npm', ['run', 'test'], {
+    stdio: 'inherit',
+  });
+  spawn.sync('npm', ['run', 'build'], {
     stdio: 'inherit',
   });
 }
