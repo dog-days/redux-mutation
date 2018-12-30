82503a58a83bbaaf1979e54b3b08642ae52a46d0
xianshannan <xianshannan@qq.com>
2018-12-30 18:41:52 +0800

    chore(postinstall): change postinsall command to all packages

--- postinstall.js
+++ postinstall.js
@@ -2,15 +2,15 @@ const fs = require('fs');
 const path = require('path');
 const spawn = require('cross-spawn');
 
-//根据是否存在.gitignore判断是否postinstall，发布后.gitignore不会一起发布
+// 根据是否存在.gitignore判断是否postinstall，发布后.gitignore不会一起发布
 if (fs.existsSync(path.resolve(process.cwd(), '.gitignore'))) {
   spawn.sync('npm', ['run', 'bootstrap'], {
     stdio: 'inherit',
   });
-  spawn.sync('npm', ['run', 'test'], {
+  spawn.sync('npm', ['run', 'test-all'], {
     stdio: 'inherit',
   });
-  spawn.sync('npm', ['run', 'build'], {
+  spawn.sync('npm', ['run', 'build-all'], {
     stdio: 'inherit',
   });
 }
