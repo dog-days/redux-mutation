b5b2cfe60fd4fa5560180da0b0829ea76e7c06d7
xianshannan <xianshannan@qq.com>
2018-12-31 15:12:36 +0800

    feat: seperator the loading state, just like dva
    BREAKING CHANGE: It not compatible with the old version. It is a new feature like dva loading.

--- packages/redux-mutation-loading/src/index.js
+++ packages/redux-mutation-loading/src/index.js
@@ -1,73 +1,66 @@
 /**
  * loading plugin 不像 redux-mutation 一样考虑 async 这些新语法转换后的代码量
  * 直接使用 @babel/runtime 处理, 代码逻辑会更直观
+ * 跟 dva-loading 比较像，但是用法还是有区别的
  */
 
 import { SEPARATOR } from 'redux-mutation';
 
-import isPlainObject from './utils/isPlainObject';
+export const LOADINGNAMESPACE = '$loading';
 
-let currentActionLoadingType;
-let currentNamespace;
+export default function createLoadingPlugin(options = {}) {
+  const { namespace = LOADINGNAMESPACE } = options;
+  const LOADINGSHOW = `${namespace}${SEPARATOR}show`;
+  const LOADINGHIDE = `${namespace}${SEPARATOR}hide`;
 
-const STARTLOADINGNAMESPACESUFFIXNAME = `${SEPARATOR}start-loading`;
-const ENDLOADINGNAMESPACESUFFIXNAME = `${SEPARATOR}end-loading`;
+  return {
+    centerEnhancer: function(center, { put }, currentMutation, actionType) {
+      const currentNamespace = currentMutation.namespace;
 
-export const LOADINGFIELDNAME = '$loading';
+      return async (action, ...otherArgs) => {
+        const { [namespace]: shouldDispatchLoading = true } = action;
 
-export default {
-  centerEnhancer: function(center, { put }, currentMutation, actionType) {
-    return async (action, ...otherArgs) => {
-      const { initialState } = currentMutation;
-      const { [LOADINGFIELDNAME]: loading = true } = action;
-      let result;
+        let result;
 
-      // 只有开启了 loading 功能（默认开启），而且 initialState 是 plain object
-      // 才可以使用 loading。
-      if (loading && isPlainObject(initialState)) {
-        // 默认都会执行 loading 逻辑，可以手动关掉，如下：
-        // dispatch( { type: 'xxx', payload: { [LOADINGFIELDNAME]: false } }
-        // 有些请求还是没必要使用 loading-plugin 的。
-        currentNamespace = currentMutation.namespace;
-        currentActionLoadingType = actionType + STARTLOADINGNAMESPACESUFFIXNAME;
-        await put({
-          type: currentActionLoadingType,
-          payload: {
-            [LOADINGFIELDNAME]: true,
-          },
-        });
-        result = await center(action, ...otherArgs);
-        // console.log(currentActionLoadingType, 111);
-        // 防止中途currentActionLoadingType和currentNamespace被其他dispatch覆盖。
-        currentActionLoadingType = actionType + ENDLOADINGNAMESPACESUFFIXNAME;
-        currentNamespace = currentMutation.namespace;
-        // console.log(currentActionLoadingType, 222);
-        await put({
-          type: currentActionLoadingType,
-          payload: {
-            [LOADINGFIELDNAME]: false,
-          },
-        });
-      } else {
-        delete action[LOADINGFIELDNAME];
-        result = await center(action, ...otherArgs);
-      }
-      return result;
-    };
-  },
-  reducerEnhancer: function(originalReducer) {
-    return (state, action) => {
-      let newState = {};
-      switch (action.type) {
-        case currentActionLoadingType:
-          newState[currentNamespace] = {
-            ...state[currentNamespace],
-            ...action.payload,
+        // 只有开启了 loading 功能（默认开启）
+        // 通过 dispatch( { type: 'xxx', [LOADINGNAMESPACE]: false })
+        if (shouldDispatchLoading) {
+          // loading 状态设置为 true
+          await put({
+            type: LOADINGSHOW,
+            payload: {
+              namespace: currentNamespace,
+              loading: true,
+            },
+          });
+
+          result = await center(action, ...otherArgs);
+
+          // loading 状态设置为 false
+          await put({
+            type: LOADINGHIDE,
+            payload: {
+              namespace: currentNamespace,
+              loading: false,
+            },
+          });
+        } else {
+          delete action[namespace];
+          result = await center(action, ...otherArgs);
+        }
+        return result;
+      };
+    },
+    extraReducers: {
+      [namespace](state = {}, { type, payload }) {
+        if (type === LOADINGSHOW || type === LOADINGHIDE) {
+          return {
+            [payload.namespace]: payload.loading,
           };
-          break;
-        default:
-      }
-      return originalReducer({ ...state, ...newState }, action);
-    };
-  },
-};
+        } else {
+          return state;
+        }
+      },
+    },
+  };
+}
